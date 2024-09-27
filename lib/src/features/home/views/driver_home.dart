cc9c0062b75e590a7986539fd6f55a7ed02fb3b4
fatima-tehreem09 <146054314+fatima-tehreem09@users.noreply.github.com>
2024-09-27 17:07:12 +0500

    location provider all done

--- lib/src/features/home/views/driver_home.dart
+++ lib/src/features/home/views/driver_home.dart
@@ -1,14 +1,12 @@
 import 'package:abiola_along_client_app/src/features/home/widgets/bottomsheet_tag_data.dart';
-import 'package:abiola_along_client_app/src/utils/perimission_location.dart';
 import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:geocoding/geocoding.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../../../const/colors.dart';
 import '../../../core/local/local_storage_repository.dart';
+import '../../../utils/google_map_util.dart';
 import '../../../widgets/app_bar.dart';
 import '../../../widgets/inter_text.dart';
 
@@ -20,146 +18,114 @@ class DriverHome extends ConsumerStatefulWidget {
 }
 
 class _DriverHomeState extends ConsumerState<DriverHome> {
-  final CustomInfoWindowController _customInfoWindowController =
-      CustomInfoWindowController();
-  final LatLng _latLng = const LatLng(30.201106652712188, 71.5038758);
-  final double _zoom = 15.0;
-  String _currentAddress = "....";
-  Position? _currentPosition;
-
   @override
   void initState() {
     super.initState();
-    _getCurrentPosition();
-  }
-
-  @override
-  void dispose() {
-    _customInfoWindowController.dispose();
-    super.dispose();
-  }
-
-  Future<void> _getCurrentPosition() async {
-    final hasPermission = await ref
-        .read(locationPermissionProvider)
-        .handleLocationPermission(context);
-    if (!hasPermission) return;
-    try {
-      final position = await Geolocator.getCurrentPosition(
-          locationSettings:
-              const LocationSettings(accuracy: LocationAccuracy.high));
-      setState(() {
-        _currentPosition = position;
-        _getAddressFromLatLng(position);
-      });
-      _animateToCurrentLocation(position);
-    } catch (e) {
-      debugPrint(e.toString());
-    }
-  }
-
-  void _animateToCurrentLocation(Position position) {
-    _customInfoWindowController.googleMapController?.animateCamera(
-      CameraUpdate.newLatLng(
-        LatLng(position.latitude, position.longitude),
-      ),
-    );
-  }
-
-  Future<void> _getAddressFromLatLng(Position position) async {
-    try {
-      final placemarks =
-          await placemarkFromCoordinates(position.latitude, position.longitude);
-      final place = placemarks[0];
-      setState(() {
-        _currentAddress =
-            "${place.street}, ${place.subLocality}, ${place.locality}, ${place.subAdministrativeArea}, ${place.postalCode}";
-      });
-    } catch (e) {
-      debugPrint(e.toString());
-    }
+    ref.read(locationNotifierProvider.notifier).getCurrentPosition();
   }
 
   @override
   Widget build(BuildContext context) {
-    final _ = ref.watch(localDataProvider);
-    final bool isDriver = _.getUserType == "driver";
+    final bool isDriver = ref.watch(localDataProvider).getUserType == "driver";
+    final location = ref.watch(locationNotifierProvider);
+    final state = ref.read(locationNotifierProvider.notifier);
     return Scaffold(
       extendBodyBehindAppBar: true,
       backgroundColor: AppColors.primaryScaffoldBg,
       appBar: AppBarWidget(
-        location: _currentAddress,
-        onTap: _getCurrentPosition,
+        location: location.currentAddress ?? "....",
+        onTap: state.getCurrentPosition,
         isDriver: isDriver,
         isHomeView: true,
         title: "Home",
       ),
-      body: Stack(
-        children: <Widget>[
-          GoogleMap(
-            onTap: (position) {
-              _customInfoWindowController.hideInfoWindow!();
-            },
-            onCameraMove: (position) {
-              _customInfoWindowController.onCameraMove!();
-            },
-            onMapCreated: (GoogleMapController controller) async {
-              _customInfoWindowController.googleMapController = controller;
-              controller.showMarkerInfoWindow(const MarkerId("marker_id"));
-              if (_currentPosition != null) {
-                _animateToCurrentLocation(_currentPosition!);
-              }
-            },
-            markers: {
-              Marker(
-                markerId: const MarkerId("marker_id"),
-                position: _latLng,
-                onTap: () {
-                  _customInfoWindowController.addInfoWindow!(
-                    InkWell(
+      body: location.initialLatLng == null
+          ? const Center(
+              child: CircularProgressIndicator(
+                color: AppColors.primaryBlue,
+              ),
+            )
+          : Stack(
+              children: <Widget>[
+                GoogleMap(
+                  onTap: (position) {
+                    state.customInfoWindowController.hideInfoWindow!();
+                  },
+                  onCameraMove: (position) {
+                    state.customInfoWindowController.onCameraMove!();
+                  },
+                  onCameraIdle: () async {
+                    final position = await state
+                        .customInfoWindowController.googleMapController
+                        ?.getLatLng(
+                      ScreenCoordinate(
+                        x: MediaQuery.of(context).size.width ~/ 2,
+                        y: MediaQuery.of(context).size.height ~/ 2,
+                      ),
+                    );
+                    if (position != null) {
+                      state.updateCurrentAddress(position);
+                    }
+                  },
+                  onMapCreated: (GoogleMapController controller) async {
+                    state.customInfoWindowController.googleMapController =
+                        controller;
+                    controller
+                        .showMarkerInfoWindow(const MarkerId("marker_id"));
+                    if (location.currentPosition != null) {
+                      state.animateToCurrentLocation(location.currentPosition!);
+                    }
+                  },
+                  markers: {
+                    Marker(
+                      markerId: const MarkerId("marker_id"),
+                      position: location.initialLatLng!,
                       onTap: () {
-                        showModalBottomSheet(
-                          useRootNavigator: false,
-                          isScrollControlled: true,
-                          enableDrag: true,
-                          context: context,
-                          builder: (_) {
-                            return const BottomSheetTagData();
-                          },
+                        state.customInfoWindowController.addInfoWindow!(
+                          InkWell(
+                            onTap: () {
+                              showModalBottomSheet(
+                                useRootNavigator: false,
+                                isScrollControlled: true,
+                                enableDrag: true,
+                                context: context,
+                                builder: (_) {
+                                  return const BottomSheetTagData();
+                                },
+                              );
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 5),
+                              decoration: BoxDecoration(
+                                color: const Color(0xff1F2937),
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              child: Center(
+                                child: InterText(
+                                  "3.5 Miles",
+                                  fontWeight: FontWeight.w400,
+                                  color: AppColors.primaryWhite,
+                                  size: 12,
+                                ),
+                              ),
+                            ),
+                          ),
+                          location.initialLatLng!,
                         );
                       },
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 10, vertical: 5),
-                        decoration: BoxDecoration(
-                          color: const Color(0xff1F2937),
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                        child: Center(
-                          child: InterText(
-                            "3.5 Miles",
-                            fontWeight: FontWeight.w400,
-                            color: AppColors.primaryWhite,
-                            size: 12,
-                          ),
-                        ),
-                      ),
                     ),
-                    _latLng,
-                  );
-                },
-              ),
-            },
-            initialCameraPosition: CameraPosition(
-              target: _latLng,
-              zoom: _zoom,
+                  },
+                  initialCameraPosition: CameraPosition(
+                    target: location.initialLatLng ?? const LatLng(0, 0),
+                    zoom: location.zoom ?? 14,
+                  ),
+                ),
+                CustomInfoWindow(
+                  controller: state.customInfoWindowController,
+                ),
+              ],
             ),
-          ),
-          CustomInfoWindow(
-            controller: _customInfoWindowController,
-          ),
-        ],
-      ),
     );
   }
 }
--- lib/src/utils/perimission_location.dart
+++ lib/src/utils/perimission_location.dart
@@ -1,5 +1,3 @@
-import 'package:abiola_along_client_app/src/widgets/error_dialog.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -8,31 +6,31 @@ final locationPermissionProvider = Provider<LocationPermissionUtil>((ref) {
 });
 
 class LocationPermissionUtil {
-  Future<bool> handleLocationPermission(BuildContext context) async {
+  Future<bool> handleLocationPermission() async {
     bool serviceEnabled;
     LocationPermission permission;
 
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      const ErrorDialog(
-        error: "Location services are disabled. Please enable the services",
-      ).show(context);
-    }
+    // if (!serviceEnabled) {
+    //   const ErrorDialog(
+    //     error: "Location services are disabled. Please enable the services",
+    //   ).show(context);
+    // }
     permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        const ErrorDialog(
-          error: "Location permissions are denied",
-        ).show(context);
-      }
+      // if (permission == LocationPermission.denied) {
+      //   const ErrorDialog(
+      //     error: "Location permissions are denied",
+      //   ).show(context);
+      // }
     }
-    if (permission == LocationPermission.deniedForever) {
-      const ErrorDialog(
-        error:
-            "Location permissions are permanently denied, we cannot request permissions.",
-      ).show(context);
-    }
+    // if (permission == LocationPermission.deniedForever) {
+    //   const ErrorDialog(
+    //     error:
+    //         "Location permissions are permanently denied, we cannot request permissions.",
+    //   ).show(context);
+    // }
     return true;
   }
 }
